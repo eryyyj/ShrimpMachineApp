40cbb83839d7d466c2f3ebda63e41082788cc088
Ery Jay Pisalbon <115686289+eryyyj@users.noreply.github.com>
2025-10-22 02:35:22 +0800

    ran script

---
+++ run_shrimp_app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# --- Load environment variables ---
+export MONGO_URI="mongodb+srv://qajgvalencia:BUxIhYb4nDlfH4DV@cluster0.h07iggq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export MONGO_DB="test"
+export QT_QPA_PLATFORM="wayland"
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export QT_SCALE_FACTOR=1
+export QT_FONT_DPI=96
+
+# --- Move to project folder ---
+cd /home/hiponpd/Documents/GitHub/ShrimpMachineApp
+
+# --- Run the app using your virtual environment ---
+/home/hiponpd/Documents/GitHub/ShrimpMachineApp/hipon-venv/bin/python3 app.py
